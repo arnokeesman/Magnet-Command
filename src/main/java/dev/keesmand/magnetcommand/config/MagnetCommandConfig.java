2857a5a12d0761b3571743fc6dd7a4bbf60b75d1
ArnoKeesmand <arnokeesman@gmail.com>
2023-08-25 15:50:20 +0200

    feat: includeContainerItems

--- src/main/java/dev/keesmand/magnetcommand/config/MagnetCommandConfig.java
+++ src/main/java/dev/keesmand/magnetcommand/config/MagnetCommandConfig.java
@@ -10,13 +10,15 @@ public class MagnetCommandConfig {
 	public final MoveMode moveMode;
 	public final boolean onBreakEnabled;
 	public final DropMode dropLocation;
+	public final boolean includeContainerItems;
 
-	public MagnetCommandConfig(int permissionLevel, boolean rangeEnabled, int range, MoveMode moveMode, boolean onBreakEnabled, DropMode dropLocation) {
+	public MagnetCommandConfig(int permissionLevel, boolean rangeEnabled, int range, MoveMode moveMode, boolean onBreakEnabled, DropMode dropLocation, boolean includeContainerItems) {
 		this.permissionLevel = permissionLevel;
 		this.rangeEnabled = rangeEnabled;
 		this.range = range;
 		this.moveMode = moveMode;
 		this.onBreakEnabled = onBreakEnabled;
 		this.dropLocation = dropLocation;
+		this.includeContainerItems = includeContainerItems;
 	}
 }
--- src/main/java/dev/keesmand/magnetcommand/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/keesmand/magnetcommand/mixin/ServerPlayerEntityMixin.java
@@ -1,27 +1,54 @@
 package dev.keesmand.magnetcommand.mixin;
 
-import com.mojang.authlib.GameProfile;
+import dev.keesmand.magnetcommand.MagnetCommandMod;
+import dev.keesmand.magnetcommand.config.MagnetCommandConfig;
+import dev.keesmand.magnetcommand.enums.MagnetMode;
+import dev.keesmand.magnetcommand.enums.MoveMode;
+import dev.keesmand.magnetcommand.util.IEntityDataSaver;
+import dev.keesmand.magnetcommand.util.Magnet;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static dev.keesmand.magnetcommand.util.Magnet.ApplyMagnetEffect;
+import java.util.List;
+
+import static dev.keesmand.magnetcommand.util.Magnet.PullItem;
+import static dev.keesmand.magnetcommand.util.Magnet.TeleportItem;
+import static dev.keesmand.magnetcommand.util.MagnetModeData.getMagnetMode;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-
-	// this is just so we have access to `this`
-	public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
-		super(world, pos, yaw, profile);
-	}
-
+public abstract class ServerPlayerEntityMixin {
 	@Inject(method = "tick", at = @At("HEAD"))
-	private void beforeTick(CallbackInfo callbackInfo) {
-		ApplyMagnetEffect(this);
+	private void magnetRangeMode(CallbackInfo callbackInfo) {
+		// well this sure is a nice trick, thanks u/tom_the_geek
+		// https://www.reddit.com/r/fabricmc/comments/nw3rs8/comment/h17daen
+		PlayerEntity player = (PlayerEntity)(Object)this;
+
+		MagnetCommandConfig config = MagnetCommandMod.CONFIG;
+		if (config == null) return;
+		if (!config.rangeEnabled) return;
+		MagnetMode mode = getMagnetMode((IEntityDataSaver) player);
+		if (mode != MagnetMode.Range) return;
+
+		double range = config.range;
+
+		// get items within range
+		Vec3d playerPos = player.getPos();
+		Box box = new Box(
+				playerPos.x+range, playerPos.y+range, playerPos.z+range,
+				playerPos.x-range, playerPos.y-range, playerPos.z-range);
+		List<ItemEntity> items = player.getWorld().getEntitiesByType(EntityType.ITEM, box, Magnet::TestItemEntity);
+
+		items.forEach(item -> {
+			if (config.moveMode == MoveMode.Pull) PullItem(playerPos, item, range);
+			else if (config.moveMode == MoveMode.Teleport) TeleportItem(playerPos, item);
+		});
 	}
 }
